4574fb37a98b14162cd9224ed984367e8cae88ad
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-04-02 18:04:12 +0300

    map display project initiated, dependencies installed, svg component created, first map display successful

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import USAMap from "./components/USAMap";
+//import USAMap2 from "./components/USAMap2";
+//import USAMap3 from "./components/USAMap3";
+import MapComponent from "./components/svg/MapComponent";
+
+function App() {
+  return (
+    <div className="App">
+      < MapComponent />
+    </div>
+  );
+}
+
+export default App;
