9c948c8be4b4abd6b9988c3b8d41dc68ad0a0d3f
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-09-02 14:35:37 +0300

    Turkey data error fix

--- client/src/components/main/Homepage.js
+++ client/src/components/main/Homepage.js
@@ -70,7 +70,7 @@ function Homepage() {
                       <td>{formatNumber(85279553)}</td>
                       <td>{formatNumber(84680273)}</td>
                       <td>{formatNumber(78741053)}</td>
-                      <td>{formatNumber(74724269)}</td>
+                      <td>74724269</td>
                       <td>{formatNumber(70586256)}</td>
                   </tr>
                   {message.map((province, index) => (
