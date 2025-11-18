009022a31171fcf7985209fc29120664c46d470d
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-11-18 11:54:47 +0300

    redirection page created

--- client/src/store/useStore.js
+++
@@ -1,9 +0,0 @@
-// src/store/useStore.js
-import { create } from 'zustand'; // Use named import
-
-const useStore = create((set) => ({
-  provincePopulation: 0,
-  setProvincePopulation: (newPopulation) => set({ provincePopulation: newPopulation }),
-}));
-
-export default useStore;
